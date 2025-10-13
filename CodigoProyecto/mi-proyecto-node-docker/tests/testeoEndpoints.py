18891eb12b55f1a4a6506681d569c27fb0663097
iaemarti <116932182+iaemarti@users.noreply.github.com>
2025-10-13 19:05:31 -0300

    Update testeoEndpoints.py

--- CodigoProyecto/mi-proyecto-node-docker/tests/testeoEndpoints.py
+++ CodigoProyecto/mi-proyecto-node-docker/tests/testeoEndpoints.py
@@ -12,7 +12,7 @@ class TestCursosEndpoints(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        print("\nFinalizado pruebas de endpoint GET de cursos.\n")
+        print("\nFinalizado pruebas del Endpoint 2.\n")
 
     def test_listar_cursos_ordenados(self):
         r = requests.get(self.CURSOS_URL, timeout=10)
@@ -46,52 +46,53 @@ class TestCursosEndpoints(unittest.TestCase):
 
         self.assertEqual(body.get("error"), "Curso no encontrado")
 
+
 class TestEndpointProfesor(unittest.TestCase):
-	URL = "http://localhost:4000/api/profesores"
+    URL = "http://localhost:4000/api/profesores"
 
-	@classmethod
-	def setUpClass(cls):
-		print("\nIniciando el proceso de pruebas.")
-		requests.delete(cls.URL)
+    @classmethod
+    def setUpClass(cls):
+        print("\nIniciando pruebas del Enpoint 1.")
+        requests.delete(cls.URL)
 
-		data = {
-			"nombre": "Profesor",
-			"email": "profesor@profesor.com",
-			"password": "1234",
-			"asignatura": "Matemáticas"
-		}
+        data = {
+            "nombre": "Profesor",
+            "email": "profesor@profesor.com",
+            "password": "1234",
+            "asignatura": "Matemáticas"
+        }
 
-		response = requests.post(cls.URL, json = data)
-		assert response.status_code == 201
-		print( "Profesor creado" )
+        response = requests.post(cls.URL, json = data)
+        assert response.status_code == 201
+        print( "Profesor creado" )
 
-	def test_profesorValido(self):
-		data = {
-			"nombre": "Martina Alvarez",
-			"email": "martina@profesor.com",
-			"password": "5678",
-			"asignatura": "Matemáticas"
-		}
+    def test_profesorValido(self):
+        data = {
+            "nombre": "Martina Alvarez",
+            "email": "martina@profesor.com",
+            "password": "5678",
+            "asignatura": "Matemáticas"
+        }
 
-		response = requests.post(self.URL, json = data)
-		self.assertEqual(response.status_code, 201)
-		self.assertIn("email", response.json())
+        response = requests.post(self.URL, json = data)
+        self.assertEqual(response.status_code, 201)
+        self.assertIn("email", response.json())
 
-	def test_profesorSinCampos(self):
-		data = {
-			"nombre": "Joaquín Quezada",
-			"asignatura": "Lenguaje"
-		}
+    def test_profesorSinCampos(self):
+        data = {
+            "nombre": "Joaquín Quezada",
+            "asignatura": "Lenguaje"
+        }
 
-		response = requests.post(self.URL, json = data)
-		self.assertEqual(response.status_code, 400)
-		self.assertIn("error", response.json())
+        response = requests.post(self.URL, json = data)
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("error", response.json())
 
-	@classmethod
-	def tearDownClass(cls):
-		requests.delete(cls.URL)
-		print("\nProceso de Pruebas Finalizado: Pofesores eliminados correctamente.")
-
+    @classmethod
+    def tearDownClass(cls):
+        requests.delete(cls.URL)
+        print("\nPofesores eliminados correctamente.")
+        print("Proceso de pruebas finalizado.")
 
 if __name__ == "__main__":
     unittest.main(verbosity = 2)
